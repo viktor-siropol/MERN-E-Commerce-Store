16d971eb8c8967bcece5a776a3016437dd97e397
Viktor Siropol <viktor.siropol.w@gmail.com>
2025-11-29 23:25:22 +0100

    Product logic

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -21,7 +21,7 @@ const productSchema = mongoose.Schema({
   quantity: {type: Number, required: true},
   category: {type: ObjectId, ref: "Category", required: true},
   description: {type: String, required: true},
-  reviewSchema: [reviewSchema],
+  reviews: [reviewSchema],
   rating: { type: Number, required: true, default: 0 },
   numReviews: { type: Number, required: true, default: 0 },
   price: { type: Number, required: true, default: 0 },
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,34 @@ const router = express.Router();
 // controllers
 import {
   addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductsById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProducts,
 } from "../controllers/productController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 
 router
   .route("/")
+  .get(fetchProducts)
   .post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route('/allproducts').get(fetchAllProducts)
+
+router
+  .route("/:id/reviews")
+  .post(authenticate, addProductReview);
+
+router.get('/top', fetchTopProducts);
+
+router
+  .route("/:id")
+  .get(fetchProductsById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, formidable(), removeProduct);
 
 export default router;
